311097cc76751a6ebaea0444fa622365d39cf3f7
rajesh <prorajeshsarkar@gmail.com>
2024-10-01 11:58:29 +0530

    first  commit

---
+++ Server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => console.log('MongoDB connected'))
+   .catch(err => console.log(err));
+
+const TodoSchema = new mongoose.Schema({
+  text: String,
+  completed: { type: Boolean, default: false }
+});
+const Todo = mongoose.model('Todo', TodoSchema);
+
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post('/todos', async (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text
+  });
+  const savedTodo = await newTodo.save();
+  res.json(savedTodo);
+});
+
+app.put('/todos/:id', async (req, res) => {
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTodo);
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Todo deleted' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
